1271f22f682b6825bcb894ec4eff4c1490499ded
macaury carvalho <carvalhomacaury@gmail.com>
2022-11-15 22:11:22 -0300

    criacao da interface responsiva da tela inicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/formulario.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function formulario( Nivel) {
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default formulario;
